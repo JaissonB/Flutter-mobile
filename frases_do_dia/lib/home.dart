7d53f85a62bb428c0ab0d96208a15e34a9533464
JaissonB <097319@aluno.uricer.edu.br>
2022-08-18 09:18:50 -0300

    Add project frases_do_dia

---
+++ frases_do_dia/lib/home.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+//stateLess não tem alteração na view, página statica
+//stateFull altera dados
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var bufferNum = null;
+  final _frases = [
+    "Não deixe para amanhã o que você pode semana que vem!",
+    "Muito ajuda quem atrapalha!",
+    "É só uma fase ruim, logo vai piorar!",
+    "Lute como nunca, perca como sempre!",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase.";
+
+  void _gerarFrase() {
+    var numeroAleatorio = Random().nextInt(_frases.length);
+    
+    while (numeroAleatorio == bufferNum) {
+      numeroAleatorio = Random().nextInt(_frases.length);
+    }
+    bufferNum = numeroAleatorio;
+
+    setState(() {
+      _fraseGerada = _frases[numeroAleatorio];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Frases do dia"),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("images/frasesDoDia.png"),
+              Text(_fraseGerada,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  elevation: 0,
+                  fixedSize: Size(220.0, 80.0),
+                  
+                ),
+                onPressed: _gerarFrase,
+                child: Text(
+                  "Nova frase",
+                  style: TextStyle(
+                    fontSize: 25
+                  ),
+                )
+              )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
---
+++ frases_do_dia/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:frases_do_dia/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
